7d6a60c0030b606294515f53a1a672d9b657bfd4
Richard <charichard09@gmail.com>
2022-10-28 09:42:42 -0700

    add code to convertBeepBoopWont to pass TDD Test3

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
 // Business Logic
 
 
-// Test2: "It should for..of loop through rangeArray and console.log(element) to confirm loop success"
+// Test3: 'It should check if element contains a 3, replace element to "Won't you be my neighbor?" and push to let beepBoopWontArray, else continue'
 // Code:
 // const inputRange = 3;
+// let beepBoopWontArray = [];
 // convertToBeepBoopWont(inputRange)
-// Expected Output: 
-// 0
-// 1
-// 2
-// 3
+// Expected Output: [0, 1, 2, "Won't you be my neighbor?"]
 
 // input for DevTools. To be passed by UI logic in the future.
 const inputRange = 3;
 
 function convertToBeepBoopWont(inputRange) {
   let rangeArray = [];
+  let beepBoopWontArray = [];
+  const threeRegExpPattern = new RegExp(/\d*3/);
 
   for (i = 0; i <= inputRange; i++) {
     rangeArray.push(i)
   }
 
   for (const element of rangeArray) {
-    console.log(element);
+    if (element.toString().match(threeRegExpPattern)) {
+      beepBoopWontArray.push("Won't you be my neighbor?");
+    } else {
+      beepBoopWontArray.push(element);
+    };
   }
 
-  return undefined;
+  return beepBoopWontArray;
 }
 
 //call function to test in DevTools
